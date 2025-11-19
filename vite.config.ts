97ab7623515e8d9e71a1b8fe50cfc55426b647a0
David Sulava <sulavaperm@gmail.com>
2025-11-19 18:26:58 +0500

    added base to config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
     // Use the loaded variables in your config
     return {
         plugins: [vue(), tailwindcss()],
+        base: '/vue-chat-ai-ui',
         server: {
             proxy: {
                 '/api': {
