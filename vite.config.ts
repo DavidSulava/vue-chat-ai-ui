55825f0f32e11b59ed40d1895a834aca6952823c
David Sulava <sulavaperm@gmail.com>
2025-11-20 16:18:08 +0500

    removed base from config

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     const {VITE_API_URL} = loadEnv(mode, process.cwd(), '');
-    const isDev = mode === 'development';
 
     return {
         plugins: [vue(), tailwindcss()],
-        base: isDev? '/' : '/vue-chat-ai-ui/',
         resolve: {
             alias: {
                 '@': resolve(__dirname, 'src'),
