f91da99b09b11c2b86672b5c695c5fb2c335b9e3
David Sulava <sulavaperm@gmail.com>
2025-11-20 15:33:29 +0500

    added deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-    // Load environment variables from the root directory
     const {VITE_API_URL} = loadEnv(mode, process.cwd(), '');
+    const isDev = mode === 'development';
 
-    // Use the loaded variables in your config
     return {
         plugins: [vue(), tailwindcss()],
-        base: '/vue-chat-ai-ui/',
+        base: isDev? '/' : '/vue-chat-ai-ui/',
         resolve: {
             alias: {
                 '@': resolve(__dirname, 'src'),
-                '#root': resolve(__dirname)
+                '#root': resolve(__dirname),
             }
         },
         server: {
