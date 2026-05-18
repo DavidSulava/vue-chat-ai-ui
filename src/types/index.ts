39a2028b5f7b6a9e28753e01295efaac6c54c819
David Sulava <sulavaperm@gmail.com>
2026-05-18 20:55:56 +0500

    types: convert index.ts to barrel re-export file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,42 +1,4 @@
-export interface ChatMessage {
-  message: string
-  reply: string
-}
-
-export interface FormattedMessage {
-  id: string
-  role: 'user' | 'ai'
-  content: string
-}
-
-export interface UserData {
-  userId: string
-  name: string
-}
-
-export interface RegisterUserRequest {
-  name: string
-  email: string
-}
-
-export interface RegisterUserResponse {
-  userId: string
-  name: string
-}
-
-export interface GetMessagesRequest {
-  userId: string
-}
-
-export interface GetMessagesResponse {
-  messages: ChatMessage[]
-}
-
-export interface ChatRequest {
-  message: string
-  userId: string
-}
-
-export interface ChatResponse {
-  reply: string
-}
+export * from './api'
+export * from './chat'
+export * from './i18n'
+export * from './user'
