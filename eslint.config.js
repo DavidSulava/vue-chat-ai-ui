19bea7f5c4895d7c14e2b9f0d99c394131b3fb0e
David Sulava <sulavaperm@gmail.com>
2026-05-18 14:50:21 +0500

    Add ESLint 10.4.0 with Vue and TypeScript support
    - Install ESLint 10.4.0 with flat config
    - Add eslint-plugin-vue for Vue SFC linting
    - Add @typescript-eslint packages for TypeScript support
    - Add eslint-plugin-prettier and prettier for code formatting
    - Add npm scripts: lint, lint:fix
    - Create eslint.config.js with recommended configs
    - Create .prettierrc with standard formatting rules

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', '*.log']
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  ...tseslint.configs['flat/recommended'],
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser
+      }
+    }
+  },
+  eslintPluginPrettierRecommended
+]
